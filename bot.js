c5febde0d0fbbe590c68de3d54e709c00f9e621c
shivshiv3 <101645704+shivshiv3@users.noreply.github.com>
2022-03-15 13:19:31 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,31 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Title of the document</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <style> p { font-size:120%; } </style> 
-</head>
-<body>
-<div class="container">
-  <div class="row">
-    <div clss="col-sm-12">
-      <h1>Welcome to useless chat bot!</h1>
-      <br />
-      <div id="conversation">
-        <p><strong>Useless bot:</strong> Hello, how are you?</p>
-      </div>
-      <br />
-      <div class="form-group row">
-        <div class="col-sm-6">
-          <input type="text" class="form-control" id="message" placeholder="Your answer...">
-        </div>
-        <button class="btn btn-primary col-sm-2" id="sayit">Say it!</button>
-      </div>
-    </div>
-  </div>
-</div>
-<script src="bot.js"></script>
-</body>
-</html>
+function answerQuery(query) {
+  query = query.toLowerCase();
+  if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
+    return "I do not care too much about weather, I'm locked inside a data center.";
+  return "Sorry Dave, I cannot do that."
+}
+
+
+function handleSayClick() {
+  // We first get the 'message' that the user entered and 'conversation' element
+  // which we are using for showing all the conversation history
+  var message = document.getElementById('message').value.toLowerCase();
+  var conversation = document.getElementById('conversation');
+  
+  // Get answer for the given query and show the query & answer
+  var answer = answerQuery(message)
+  conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
+  conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
+}
+
+
+document.getElementById("sayit").onclick = handleSayClick;
